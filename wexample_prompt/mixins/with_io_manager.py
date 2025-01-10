0e2d03da014c661d522746659356622bba60f671
weeger <romain.weeger@gmail.com>
2025-01-10 14:13:20 +0100

    Implements io manager

--- wexample_prompt/mixins/with_io_manager.py
+++ wexample_prompt/mixins/with_io_manager.py
@@ -5,36 +5,17 @@ from wexample_prompt.io_manager import IoManager
 
 
 class WithIoManager(BaseModel):
-    """
-    Mixin that adds an IoManager property to a class.
-    The IoManager instance can be shared between multiple classes
-    or be unique to each instance.
-    """
-    
-    _io: Optional[IoManager] = Field(
+    io: Optional[IoManager] = Field(
         default=None,
         description="IoManager instance that can be injected via constructor"
     )
     
     @property
     def io(self) -> IoManager:
-        """
-        Get the IoManager instance. If none exists, creates a new one.
-        
-        Returns:
-            IoManager: The IoManager instance
-        """
-        if self._io is None:
-            self._io = IoManager()
-        return self._io
+        if self.io is None:
+            self.io = IoManager()
+        return self.io
     
     @io.setter
     def io(self, manager: IoManager) -> None:
-        """
-        Set the IoManager instance.
-        Useful for sharing the same IoManager between multiple classes.
-        
-        Args:
-            manager (IoManager): The IoManager instance to use
-        """
-        self._io = manager
+        self.io = manager
