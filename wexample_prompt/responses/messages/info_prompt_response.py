c373477c11d249dd2e38bd99f5a40e3c4d09c573
weeger <romain.weeger@gmail.com>
2025-02-01 05:34:38 +0100

    Cleanup

--- wexample_prompt/responses/messages/info_prompt_response.py
+++ wexample_prompt/responses/messages/info_prompt_response.py
@@ -9,32 +9,6 @@ if TYPE_CHECKING:
     from wexample_prompt.responses.abstract_prompt_response import AbstractPromptResponse
 
 
-class InfoPromptResponseIoManagerMixin:
-    """Mixin for IoManager to handle info responses."""
-
-    def info(self, message: str, **kwargs) -> "InfoPromptResponse":
-        """Create and display an info response."""
-        response = InfoPromptResponse.create_info(
-            message=message,
-            context=self._create_context(),
-        )
-
-        if self._logger.handlers:
-            self._logger.info(message)
-
-        self.print_response(response)
-        return response
-
-
-class InfoPromptResponsePromptContextMixin:
-    """Mixin for WithPromptContext to handle info responses with context formatting."""
-
-    def info(self, message: str, **kwargs) -> "InfoPromptResponse":
-        """Create and display an info response with context formatting."""
-        formatted_message = self.format_message(message)
-        return self.io.info(formatted_message, **kwargs)
-
-
 class InfoPromptResponse(BaseMessageResponse):
     """Response for info messages."""
     SYMBOL: ClassVar[str] = "ℹ️"
--- wexample_prompt/responses/messages/log_prompt_response.py
+++ wexample_prompt/responses/messages/log_prompt_response.py
@@ -1,41 +1,15 @@
-from typing import ClassVar, TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from wexample_prompt.common.color_manager import ColorManager
-from wexample_prompt.responses.messages.base_message_response import BaseMessageResponse
-from wexample_prompt.enums.message_type import MessageType
 from wexample_prompt.common.prompt_context import PromptContext
 from wexample_prompt.common.prompt_response_line import PromptResponseLine
 from wexample_prompt.common.prompt_response_segment import PromptResponseSegment
+from wexample_prompt.enums.message_type import MessageType
 from wexample_prompt.enums.terminal_color import TerminalColor
+from wexample_prompt.responses.messages.base_message_response import BaseMessageResponse
 
 if TYPE_CHECKING:
     from wexample_prompt.responses.abstract_prompt_response import AbstractPromptResponse
-
-
-class LogPromptResponseIoManagerMixin:
-    """Mixin for IoManager to handle log responses."""
-
-    def log(self, message: str, **kwargs) -> "LogPromptResponse":
-        """Create and display a log response."""
-        response = LogPromptResponse.create_log(
-            message=message,
-            context=self._create_context(),
-        )
-
-        if self._logger.handlers:
-            self._logger.debug(message)
-
-        self.print_response(response)
-        return response
-
-
-class LogPromptResponsePromptContextMixin:
-    """Mixin for WithPromptContext to handle log responses with context formatting."""
-
-    def log(self, message: str, **kwargs) -> "LogPromptResponse":
-        """Create and display a log response with context formatting."""
-        formatted_message = self.format_message(message)
-        return self.io.log(formatted_message, **kwargs)
 
 
 class LogPromptResponse(BaseMessageResponse):
@@ -50,20 +24,20 @@ class LogPromptResponse(BaseMessageResponse):
     ) -> "AbstractPromptResponse":
         text_lines = message.split('\n')
         lines = []
-        
+
         for text_line in text_lines:
             message_segment = PromptResponseSegment(
                 text=ColorManager.colorize(text_line, TerminalColor.WHITE)
             )
-            
+
             line = PromptResponseLine(
                 segments=[message_segment],
                 line_type=cls.get_message_type()
             )
             lines.append(line)
-        
+
         return cls(lines=lines, context=context, message_type=cls.get_message_type())
-    
+
     @classmethod
     def get_message_type(cls) -> MessageType:
         """Get the message type for log messages."""
