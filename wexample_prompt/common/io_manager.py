4a21453d7f1e1334007d0a7eda44e17202250686
weeger <romain.weeger@gmail.com>
2025-02-02 20:05:24 +0100

    Fix tests

--- wexample_prompt/common/io_manager.py
+++ wexample_prompt/common/io_manager.py
@@ -13,6 +13,8 @@ from wexample_prompt.mixins.response.manager.interactive.choice_prompt_response_
     ChoicePromptResponseManagerMixin
 from wexample_prompt.mixins.response.manager.interactive.dir_picker_prompt_response_manager_mixin import \
     DirPickerPromptResponseManagerMixin
+from wexample_prompt.mixins.response.manager.interactive.file_picker_prompt_response_manager_mixin import \
+    FilePickerPromptResponseManagerMixin
 from wexample_prompt.mixins.response.manager.interactive.progress_prompt_response_manager_mixin import \
     ProgressPromptResponseManagerMixin
 from wexample_prompt.mixins.response.manager.messages.debug_prompt_response_manager_mixin import \
@@ -35,6 +37,8 @@ from wexample_prompt.mixins.response.manager.titles.subtitle_prompt_response_man
     SubtitlePromptResponseManagerMixin
 from wexample_prompt.mixins.response.manager.titles.title_prompt_response_manager_mixin import \
     TitlePromptResponseManagerMixin
+from wexample_prompt.mixins.response.manager.data.list_prompt_response_manager_mixin import \
+    ListPromptResponseManagerMixin
 from wexample_prompt.mixins.with_indent import WithIndent
 from wexample_prompt.responses import BasePromptResponse
 from wexample_prompt.themes.default.abstract_prompt_theme import AbstractPromptTheme
@@ -52,6 +56,7 @@ class IoManager(
     ChoicePromptResponseManagerMixin,
     ChoiceDictPromptResponseManagerMixin,
     DirPickerPromptResponseManagerMixin,
+    FilePickerPromptResponseManagerMixin,
     ProgressPromptResponseManagerMixin,
     # Messages
     DebugPromptResponseManagerMixin,
@@ -135,6 +140,7 @@ class IoManager(
         from wexample_prompt.responses.interactive.choice_prompt_response import ChoicePromptResponse
         from wexample_prompt.responses.interactive.choice_dict_prompt_response import ChoiceDictPromptResponse
         from wexample_prompt.responses.interactive.dir_picker_prompt_response import DirPickerPromptResponse
+        from wexample_prompt.responses.interactive.file_picker_prompt_response import FilePickerPromptResponse
         from wexample_prompt.responses.interactive.progress_prompt_response import ProgressPromptResponse
 
         return [
@@ -143,6 +149,7 @@ class IoManager(
             ChoicePromptResponse,
             ChoiceDictPromptResponse,
             DirPickerPromptResponse,
+            FilePickerPromptResponse,
             ProgressPromptResponse,
             # Messages
             LogPromptResponse,
