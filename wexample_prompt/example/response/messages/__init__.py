438c9e0e163b423e314fcedecb39d2915495f660
weeger <romain.weeger@gmail.com>
2025-02-02 10:53:40 +0100

    Example

---
+++ wexample_prompt/example/response/messages/__init__.py
@@ -0,0 +1,10 @@
+"""Example classes for message responses."""
+from wexample_prompt.example.response.messages.debug_example import DebugExample
+from wexample_prompt.example.response.messages.info_example import InfoExample
+from wexample_prompt.example.response.messages.log_example import LogExample
+
+__all__ = [
+    'DebugExample',
+    'InfoExample',
+    'LogExample',
+]
