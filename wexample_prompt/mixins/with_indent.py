8fba1760aa62df5e09bfc0166a9d06cb590bcfa2
weeger <romain.weeger@gmail.com>
2025-08-07 16:45:22 +0200

    Style

--- wexample_prompt/mixins/with_indent.py
+++ wexample_prompt/mixins/with_indent.py
@@ -2,11 +2,11 @@ class WithIndent:
     log_indent: int = 0
     _indent_string: str = "  "
 
-    def log_indent_up(self) -> None:
-        self.log_indent += 1
+    def log_indent_up(self, number: int = 1) -> None:
+        self.log_indent += number
 
-    def log_indent_down(self) -> None:
-        self.log_indent -= 1
+    def log_indent_down(self, number: int = 1) -> None:
+        self.log_indent -= number
 
     def build_indent(self, increment: int = 0) -> str:
         return str(self._indent_string * (self.log_indent + increment))
