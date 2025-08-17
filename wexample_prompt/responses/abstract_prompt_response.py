71e323359eab9dc983695bc1788fe5ac826a2ace
weeger <romain.weeger@gmail.com>
2025-08-17 16:26:18 +0200

    Create prompt context

--- wexample_prompt/responses/abstract_prompt_response.py
+++ wexample_prompt/responses/abstract_prompt_response.py
@@ -1,8 +1,8 @@
-from abc import ABC
 from typing import TYPE_CHECKING
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+from wexample_helpers.classes.extended_base_model import ExtendedBaseModel
 from wexample_helpers.classes.mixin.has_snake_short_class_name_class_mixin import HasSnakeShortClassNameClassMixin
 from wexample_prompt.common.prompt_context import PromptContext
 
@@ -10,8 +10,11 @@ if TYPE_CHECKING:
     pass
 
 
-class AbstractPromptResponse(HasSnakeShortClassNameClassMixin, BaseModel, ABC):
-    context: PromptContext = None
+class AbstractPromptResponse(HasSnakeShortClassNameClassMixin, ExtendedBaseModel):
+    context: PromptContext = Field(
+        default_factory=PromptContext,
+        description="Execution context for prompt rendering",
+    )
 
     def __init__(self, **data):
         if not "context" in data or not isinstance(data.get("context"), PromptContext):
