e34b18ba1ce18e7fafe1b0242bac61e1dae29537
weeger <romain.weeger@gmail.com>
2025-08-18 17:46:51 +0200

    List and verbosity

---
+++ wexample_prompt/example/response/data/list_example.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from wexample_prompt.example.abstract_response_example import AbstractResponseExample
+from wexample_prompt.responses.data.list_prompt_response import ListPromptResponse
+
+
+class ListExample(AbstractResponseExample):
+    def get_test_items(self) -> List[str]:
+        return [
+            "Item A",
+            "  Sub-item A1",
+            "  Sub-item A2",
+            "Item B",
+        ]
+
+    def example_manager(self):
+        self.io.list(items=self.get_test_items())
+
+    def example_class(self):
+        return ListPromptResponse.create_list(
+            items=self.get_test_items(),
+        )
+
+    def example_extended(self):
+        self._class_with_methods.list(items=self.get_test_items())
