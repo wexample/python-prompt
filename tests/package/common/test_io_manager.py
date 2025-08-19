39eb57d9e5a0f0a55a362093832d41df45620050
weeger <romain.weeger@gmail.com>
2025-08-19 13:42:00 +0200

    Progress

--- tests/package/common/test_io_manager.py
+++ tests/package/common/test_io_manager.py
@@ -2,10 +2,28 @@ from wexample_prompt.testing.abstract_prompt_test import AbstractPromptTest
 
 
 class TestIoManager(AbstractPromptTest):
-    def test_create_basic_line(self):
+    def test_echo_width(self):
         assert self._io.terminal_width is not None
 
         # Echo a string using the terminal with length
         rendered = self._io.echo(message=(self._io.terminal_width * "!"))
         # It should print only one line.
+        assert self._assert_rendered_lines_count(rendered=rendered, lines_count=1)
+
+    def test_progress_width(self):
+        # Echo a string using the terminal with length
+        rendered = self._io.progress(
+            total=20,
+            current=10
+        )
+        assert self._assert_rendered_lines_count(rendered=rendered, lines_count=1)
+
+        rendered = self._io.progress(
+            label="With a label",
+            total=20,
+            current=10
+        )
+        assert self._assert_rendered_lines_count(rendered=rendered, lines_count=1)
+
+    def _assert_rendered_lines_count(self, rendered: str, lines_count: int) -> None:
         assert len(rendered.split("\n")) == 1
