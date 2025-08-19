f352f114d29ce9ce2089f32c2f3c144c6c339b79
weeger <romain.weeger@gmail.com>
2025-08-19 13:01:23 +0200

    Prepare

--- docs/demo.sh
+++ docs/demo.sh
@@ -1,6 +1,8 @@
 
 # Local python3 should be installed
+.wex/python/venv/bin/python3 docs/examples/confirm.py
 .wex/python/venv/bin/python3 docs/examples/exception.py
 .wex/python/venv/bin/python3 docs/examples/nesting.py
+.wex/python/venv/bin/python3 docs/examples/progress.py
 .wex/python/venv/bin/python3 docs/examples/types.py
 .wex/python/venv/bin/pytest
---
+++ docs/examples/confirm.py
@@ -0,0 +1,5 @@
+from wexample_prompt.common.io_manager import IoManager
+
+if __name__ == "__main__":
+    demo_io = IoManager()
+    print('TODO test confirm')
