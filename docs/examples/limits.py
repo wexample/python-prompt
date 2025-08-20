69e3705ce2f6034855e4514aa96600b11d39b09b
weeger <romain.weeger@gmail.com>
2025-08-20 14:55:20 +0200

    Fix lines breaks

--- docs/examples/limits.py
+++ docs/examples/limits.py
@@ -1,18 +1,20 @@
 from wexample_prompt.common.io_manager import IoManager
 
+several_lines = "> And several short lines \n"
+line_breaks_text = f'This is a {"long " * 30}text\n> With a {"long " * 30} line\n{several_lines * 30}'
+
 
 def echo_long_multiline_text(io: IoManager) -> None:
-    several_lines = "> And several short lines \n"
+    io.separator("Check lines break management")
+    io.echo(line_breaks_text)
 
-    io.separator("Check lines break managerment")
-    io.echo(
-        f'This is a {"long " * 30}text\n'
-        f'> With a {"long " * 30} line\n'
-        f'{several_lines * 30}'
 
-    )
+def task_long_multiline_text(io: IoManager) -> None:
+    io.separator("Check lines break management in tasks")
+    io.task(line_breaks_text)
 
 
 if __name__ == "__main__":
     io = IoManager()
     echo_long_multiline_text(io)
+    task_long_multiline_text(io)
