be336bf669902ffe57cdafcd1a1e7e317e9590d5
weeger <romain.weeger@gmail.com>
2025-08-21 11:37:45 +0200

    Verbosity

--- tests/package/responses/titles/test_title_prompt_response.py
+++ tests/package/responses/titles/test_title_prompt_response.py
@@ -1,5 +1,8 @@
 """Tests for TitlePromptResponse (titles)."""
 
+from typing import Type
+
+from wexample_helpers.const.types import Kwargs
 from wexample_prompt.responses.abstract_prompt_response import AbstractPromptResponse
 from wexample_prompt.testing.abstract_title_prompt_response_test import AbstractTitlePromptResponseTest
 
@@ -7,13 +10,17 @@ from wexample_prompt.testing.abstract_title_prompt_response_test import Abstract
 class TestTitlePromptResponse(AbstractTitlePromptResponseTest):
     """Focused tests for TitlePromptResponse rendering and options."""
 
-    def create_test_response(self, **kwargs) -> AbstractPromptResponse:
+    def _get_response_class(self) -> Type[AbstractPromptResponse]:
         from wexample_prompt.responses.titles.title_prompt_response import (
             TitlePromptResponse,
         )
+        return TitlePromptResponse
+
+    def _create_test_kwargs(self, kwargs=None) -> Kwargs:
+        kwargs = kwargs or {}
         kwargs.setdefault("text", self._test_message)
         # keep defaults: color=CYAN, character=DEFAULT, width=None
-        return TitlePromptResponse.create_title(**kwargs)
+        return kwargs
 
     def _assert_specific_format(self, rendered: str):
         # Title uses prefix "❯" and default fill character "⫻"
@@ -27,14 +34,16 @@ class TestTitlePromptResponse(AbstractTitlePromptResponseTest):
     # Keep default common structure from AbstractPromptResponseTest (expects blank lines)
 
     def test_renders_message_and_format(self):
-        response = self.create_test_response()
+        from wexample_prompt.responses.titles.title_prompt_response import TitlePromptResponse
+        response = TitlePromptResponse.create_title(text=self._test_message)
         response.render()
         self._assert_common_response_structure(response)
         self._assert_contains_text(response.rendered_content, self._test_message)
         self._assert_specific_format(response.rendered_content)
 
     def test_custom_character(self):
-        response = self.create_test_response(character="=")
+        from wexample_prompt.responses.titles.title_prompt_response import TitlePromptResponse
+        response = TitlePromptResponse.create_title(text=self._test_message, character="=")
         rendered = response.render()
         self._assert_contains_text(rendered, self._test_message)
         self._assert_contains_text(rendered, "=")
--- wexample_prompt/mixins/response/titles/title_prompt_response_manager_mixin.py
+++ wexample_prompt/mixins/response/titles/title_prompt_response_manager_mixin.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Optional, cast
 from wexample_helpers.const.types import Kwargs
 from wexample_prompt.common.prompt_context import PromptContext
 from wexample_prompt.const.types import LineMessage
+from wexample_prompt.enums.verbosity_level import VerbosityLevel
 
 if TYPE_CHECKING:
     from wexample_prompt.enums.terminal_color import TerminalColor
@@ -13,20 +14,22 @@ if TYPE_CHECKING:
 class TitlePromptResponseManagerMixin:
     def title(
             self: "IoManager",
-            message: LineMessage,
+            text: LineMessage,
             color: Optional["TerminalColor"] = None,
             character: Optional[str] = None,
             width: Optional[int] = None,
+            verbosity: Optional[VerbosityLevel] = VerbosityLevel.DEFAULT,
             context: Optional[PromptContext] = None,
             **kwargs: Kwargs
     ) -> "AbstractPromptResponse":
         from wexample_prompt.responses.titles.title_prompt_response import TitlePromptResponse
 
         response = TitlePromptResponse.create_title(
-            text=message,
+            text=text,
             color=color,
             character=character,
             width=width,
+            verbosity=verbosity,
         )
 
         return cast("IoManager", self).print_response(
