391bcbbc6534aa5290127ccb0c7dd2f8bc978aa1
weeger <romain.weeger@gmail.com>
2025-08-21 11:27:15 +0200

    Verbosity

--- docs/examples/indentation.py
+++ docs/examples/indentation.py
@@ -1,4 +1,5 @@
 from wexample_prompt.common.io_manager import IoManager
+from wexample_prompt.common.prompt_context import PromptContext
 
 line_breaks_text = f'This is a {"long " * 80}text'
 
@@ -6,6 +7,14 @@ if __name__ == "__main__":
     io = IoManager()
 
     # Simple yes/no box (using preset mapping constant)
-    response = io.log(
+    io.log(
         message=line_breaks_text,
+    )
+
+    io.log(
+        message=line_breaks_text,
+        context=PromptContext(
+            formatting=True,
+            indentation=1,
+        )
     )
