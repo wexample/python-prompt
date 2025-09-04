0d146a76b682d37d7235a0add1119c1d1c8c32d6
weeger <romain.weeger@gmail.com>
2025-09-04 06:47:15 +0200

    Const

--- docs/examples/nesting.py
+++ docs/examples/nesting.py
@@ -8,18 +8,15 @@ if __name__ == "__main__":
 
     from wexample_prompt.common.io_manager import IoManager
     import time
-    from wexample_helpers.const.spinners import BRAILLE_SPINNER_FRAMES as SPINNER_FRAMES
+    from wexample_prompt.common.spinner_pool import SpinnerPool
     io = IoManager()
     io.separator()
 
-    frames = SPINNER_FRAMES
     start = time.time()
     total = 5.0  # seconds
     delay = 0.05  # 50 ms between frames
     while time.time() - start < total:
-        for sym in frames:
-            if time.time() - start >= total:
-                break
-            response = io.info('Loading...', symbol=sym)
-            time.sleep(delay)
-            io.erase_response(response=response)
+        sym = SpinnerPool.next()
+        response = io.info('Loading...', symbol=sym)
+        time.sleep(delay)
+        io.erase_response(response=response)
