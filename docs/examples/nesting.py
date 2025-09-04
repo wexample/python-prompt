7625d56b9a307022a7c7d9f68d89d85fe2223b86
weeger <romain.weeger@gmail.com>
2025-09-04 05:52:44 +0200

    Loading

--- docs/examples/nesting.py
+++ docs/examples/nesting.py
@@ -5,3 +5,19 @@ if __name__ == "__main__":
     level_one._init_io_manager()
 
     level_one.print_deep_log_one()
+
+    from wexample_prompt.common.io_manager import IoManager
+    import time
+    io = IoManager()
+    io.separator()
+
+    frames = ["⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽", "⣾"]
+    start = time.time()
+    total = 5.0  # seconds
+    delay = 0.05  # 50 ms between frames
+    while time.time() - start < total:
+        for sym in frames:
+            if time.time() - start >= total:
+                break
+            io.info('Loading...', symbol=sym)
+            time.sleep(delay)
