53fcf6064f3ba5d7199e6e26ea1163bb77e45e49
weeger <romain.weeger@gmail.com>
2025-09-11 09:01:25 +0200

    Last fixes

--- src/wexample_prompt/mixins/with_io_methods.py
+++ src/wexample_prompt/mixins/with_io_methods.py
@@ -5,7 +5,6 @@ from typing import Any
 from wexample_helpers.decorator.base_class import base_class
 from wexample_prompt.mixins.with_io_manager import WithIoManager
 
-
 @base_class
 class WithIoMethods(WithIoManager):
     """
@@ -17,20 +16,21 @@ class WithIoMethods(WithIoManager):
         if io_response is not None:
             return io_response
 
-        # Try to get the attribute from super(), but handle the case where
-        # super() doesn't have __getattr__ (like with attrs classes)
         try:
             return super().__getattr__(name)
         except AttributeError:
-            # If super() doesn't have __getattr__, raise AttributeError for the original attribute
             raise AttributeError(
                 f"'{self.__class__.__name__}' object has no attribute '{name}'"
             )
 
     def _get_io_methods(self, name: str) -> Any:
+        if self.io is None:
+            raise RuntimeError(
+                f"{self.__class__.__name__} requires an 'io' manager, but none was provided."
+            )
+
         if hasattr(self.io, name):
             attr = getattr(self.io, name)
-
             if callable(attr):
 
                 def wrapper(*args, **kwargs):
@@ -38,6 +38,5 @@ class WithIoMethods(WithIoManager):
                     return attr(*args, **kwargs)
 
                 return wrapper
-
             return attr
         return None
