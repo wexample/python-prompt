6d8da01868d6c9dee9bee725cfa2eea6938b8078
weeger <romain.weeger@gmail.com>
2025-09-21 09:26:58 +0200

    Test

---
+++ examples/_all.sh
@@ -0,0 +1,12 @@
+
+# Local python3 should be installed
+.wex/python/venv/bin/python3 examples/choice.py
+.wex/python/venv/bin/python3 examples/confirm.py
+.wex/python/venv/bin/python3 examples/exception.py
+.wex/python/venv/bin/python3 examples/indentation.py
+.wex/python/venv/bin/python3 examples/limits.py
+.wex/python/venv/bin/python3 examples/nesting.py
+.wex/python/venv/bin/python3 examples/progress.py
+.wex/python/venv/bin/python3 examples/screen.py
+.wex/python/venv/bin/python3 examples/types.py
+.wex/python/venv/bin/pytest
