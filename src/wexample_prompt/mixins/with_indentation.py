8de0bfd631561485501c5bc6dda5c03b9e37620e
weeger <romain.weeger@gmail.com>
2025-10-25 13:14:13 +0200

    Rework core args

--- src/wexample_prompt/mixins/with_indentation.py
+++ src/wexample_prompt/mixins/with_indentation.py
@@ -10,10 +10,19 @@ class WithIndentation:
 
     @property
     def indentation(self) -> int:
+        """Current indentation level."""
         return self._indentation
 
+    @indentation.setter
+    def indentation(self, value: int) -> None:
+        self._indentation = value
+
     def indentation_down(self, number: int = 1) -> None:
+        """Decrease indentation by a given number (default: 1)."""
         self._indentation -= number
+        if self._indentation < 0:
+            self._indentation = 0  # optional safety guard
 
     def indentation_up(self, number: int = 1) -> None:
+        """Increase indentation by a given number (default: 1)."""
         self._indentation += number
