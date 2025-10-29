d7928d3d262a3f3f13761d62fad61a30a3a52fb0
weeger <romain.weeger@gmail.com>
2025-10-29 11:15:18 +0100

    Use standard venv location

--- examples/_all.sh
+++ examples/_all.sh
@@ -1,12 +1,12 @@
 
 # Local python3 should be installed
-.wex/python/venv/bin/python3 examples/choice.py
-.wex/python/venv/bin/python3 examples/confirm.py
-.wex/python/venv/bin/python3 examples/exception.py
-.wex/python/venv/bin/python3 examples/indentation.py
-.wex/python/venv/bin/python3 examples/limits.py
-.wex/python/venv/bin/python3 examples/nesting.py
-.wex/python/venv/bin/python3 examples/progress.py
-.wex/python/venv/bin/python3 examples/screen.py
-.wex/python/venv/bin/python3 examples/types.py
-.wex/python/venv/bin/pytest
+.venv/bin/python3 examples/choice.py
+.venv/bin/python3 examples/confirm.py
+.venv/bin/python3 examples/exception.py
+.venv/bin/python3 examples/indentation.py
+.venv/bin/python3 examples/limits.py
+.venv/bin/python3 examples/nesting.py
+.venv/bin/python3 examples/progress.py
+.venv/bin/python3 examples/screen.py
+.venv/bin/python3 examples/types.py
+.venv/bin/pytest
