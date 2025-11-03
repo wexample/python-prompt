cd0f615ed323e5ba3551abc204c984730b3d0c5b
weeger <romain.weeger@gmail.com>
2025-11-03 08:59:09 +0100

    Interactive executor

--- src/wexample_prompt/example/interactive_executor.py
+++ src/wexample_prompt/example/interactive_executor.py
@@ -1,6 +1,8 @@
 from wexample_helpers.classes.example.executor import Executor
+from wexample_prompt.example.interactive_example import InteractiveExample
 from wexample_prompt.mixins.with_io_methods import WithIoMethods
 
 
 class InteractiveExecutor(WithIoMethods, Executor):
-    pass
+    def _get_example_class_type(self) -> type[InteractiveExample]:
+        return InteractiveExample
