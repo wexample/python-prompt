6dbd9abe5253d4a77ae2eea4f676f4056cd0957a
weeger <romain.weeger@gmail.com>
2025-11-04 20:48:59 +0100

    App info

--- src/wexample_prompt/example/example_class_with_methods.py
+++ src/wexample_prompt/example/example_class_with_methods.py
@@ -10,4 +10,6 @@ from wexample_prompt.testing.resources.classes.base_class_with_io_methods import
 @base_class
 class ExampleClassWithMethods(BaseClassWithIoMethods):
     def _format_context_prompt_message(self, message: str, indent: str) -> str:
+
         return f"{indent}[EXAMPLE|{self.__class__.__name__}]: {message}"
+
